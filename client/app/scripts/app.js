7f39440daf66c5e4b93cea9100a7f9778e5dd4b8
Nuno Rosa <credential.helper>
2012-11-07 01:22:15 +0000

    fix: link to engine js file

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,7 +1,7 @@
 define([], function() {
   return{ reloadEngine: function(){
   		console.log("reloading engine");
-  		require('[https://github.com/yarcub/pushtorun/raw/master/client/app/scripts/engine/pushtorun]', function(engine){
+  		require(['https://raw.github.com/yarcub/pushtorun/master/client/app/scripts/engine/pushtorun'], function(engine){
   			engine.run();
   		});
   	}

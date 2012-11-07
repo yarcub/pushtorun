d563d92f7b5a92eaaadbe2ae0650f0de5dae546f
Nuno Rosa <credential.helper>
2012-11-07 02:29:52 +0000

    avoid github cache

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -7,7 +7,7 @@ require.config({
   }
 });
  
-require(['http://pushtorun.herokuapp.com/socket.io/socket.io.js', 'app'], function(io, app) {
+require(['http://pushtorun.herokuapp.com/socket.io/socket.io.js?timestamp=' + (new Date()).getTime(), 'app'], function(io, app) {
   console.log("App initialized");
    var socket = io.connect('http://pushtorun.herokuapp.com/');
   	socket.on('update', function (data) {

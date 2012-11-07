2073a00ecdd14901f71f16f115c0cbc8ddaacc63
Nuno Rosa <credential.helper>
2012-11-07 01:23:34 +0000

    another try at the correct url =/

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,7 +1,7 @@
 define([], function() {
   return{ reloadEngine: function(){
   		console.log("reloading engine");
-  		require(['https://raw.github.com/yarcub/pushtorun/master/client/app/scripts/engine/pushtorun'], function(engine){
+  		require(['https://raw.github.com/yarcub/pushtorun/master/client/app/scripts/engine/pushtorun.js'], function(engine){
   			engine.run();
   		});
   	}

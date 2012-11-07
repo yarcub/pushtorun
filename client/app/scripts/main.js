50a04d08a8753c2240b32235e9ffe915f5bda9b2
Nuno Rosa <credential.helper>
2012-11-07 00:57:41 +0000

    first test

---
+++ client/app/scripts/main.js
@@ -0,0 +1,16 @@
+require.config({
+  shim: {
+  },
+
+  paths: {
+    jquery: 'vendor/jquery.min'
+  }
+});
+ 
+require(['http://pushtorun.herokuapp.com:80/socket.io/socket.io.js', 'app'], function(io, app) {
+  console.log("App initialized");
+   var socket = io.connect('http://pushtorun.herokuapp.com:80/');
+  	socket.on('update', function (data) {
+    	console.log(data.msg);
+  	});
+});

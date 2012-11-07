513c613756beba23ec73c0941255b40def3da7a2
Nuno Rosa <credential.helper>
2012-11-07 01:36:19 +0000

    added comment

--- client/app/scripts/engine/pushtorun.js
+++ client/app/scripts/engine/pushtorun.js
@@ -2,6 +2,7 @@ define([], function() {
   return {
   	run : function(){
   		var c = document.querySelector(".container");
+  		//this will update the client DOM when runned;
   		c.innerText = new Date() + "=>someone pushed to github";
   	}
   }

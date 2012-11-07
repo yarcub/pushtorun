a79019bd8c5f9dd3572f53443737f2f3c53dda50
Nuno Rosa <credential.helper>
2012-11-07 01:20:33 +0000

    fix: type on engine require call

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,7 +1,7 @@
 define([], function() {
   return{ reloadEngine: function(){
   		console.log("reloading engine");
-  		require('[https://github.com/yarcub/pushtorun/raw/master/client/app/scripts/engine/pushtorun.js]', function(engine){
+  		require('[https://github.com/yarcub/pushtorun/raw/master/client/app/scripts/engine/pushtorun]', function(engine){
   			engine.run();
   		});
   	}

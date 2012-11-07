1741dc145a5398b954d692fb49bfc3e6880239a0
Nuno Rosa <nuno.sp.rosa@gmail.com>
2012-11-07 02:20:32 +0000

    Update client/app/scripts/engine/pushtorun.js

--- client/app/scripts/engine/pushtorun.js
+++ client/app/scripts/engine/pushtorun.js
@@ -4,7 +4,8 @@ define([], function() {
   		console.log("engine=>run()");
   		var c = document.querySelector(".container");
   		//this will update the client DOM when runned;
-  		c.innerText = new Date() + "=>someone pushed to github";
+      c.style.background = 'yellow';
+  		c.textContent = new Date() + "=>someone pushed to github";
   	}
   }
 });

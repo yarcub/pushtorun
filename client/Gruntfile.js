7e4793b5ad0746ee92efb29d6e85448f95a21fed
Nuno Rosa <credential.helper>
2012-11-07 02:13:52 +0000

    changing ouput dir to server public folder

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -111,7 +111,7 @@ module.exports = function( grunt ) {
     // the staging directory used during the process
     staging: 'temp',
     // final build output
-    output: 'dist',
+    output: '../server/public/',
 
     mkdirs: {
       staging: 'app/'
